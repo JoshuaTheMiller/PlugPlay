4bd2e7abbb393c025531c0e059605119ee3bcf48
Joshua Miller <joshdan65@gmail.com>
2018-06-18 18:58:08 -0500

    Added download capabilities to RMF

--- RMF/popup.js
+++ RMF/popup.js
@@ -8,4 +8,14 @@ $(function () {
             chrome.tabs.sendMessage(tabs[0].id, message);
         })
     });
+
+    $("#downloadAllPayloadsAsJsonButton").click(function () {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            let message = {
+                action: "downloadAllPayloadsAsJson"
+            }
+
+            chrome.tabs.sendMessage(tabs[0].id, message);
+        })
+    });
 });
