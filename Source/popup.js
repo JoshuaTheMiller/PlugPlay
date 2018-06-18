a006b123134348c749da8e3c4fe1c3a31e9bba8c
Joshua Miller <joshdan65@gmail.com>
2018-06-17 21:14:41 -0500

    Complete code for Module 1

---
+++ Source/popup.js
@@ -0,0 +1,11 @@
+let greetCount = 0;
+
+$(function() {
+    $('#name').keyup(function() {
+        $('#greeting').text('Hi, ' + $('#name').val() + '!');
+        greetCount++;
+        chrome.browserAction.setBadgeText({
+            'text':greetCount.toString()
+        });        
+    })
+})
